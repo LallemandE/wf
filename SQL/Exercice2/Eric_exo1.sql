75046c038d1b91e3ce038874b684b581b01c8ce7
LallemandE <job@lallemand.lu>
2018-02-22 09:14:53 +0100

    exercice 2 commit

---
+++ SQL/Exercice2/Eric_exo1.sql
@@ -0,0 +1,4 @@
+select agent_code, sum(amount), count(id), avg(amount) as MEANORDER from orders 
+where agent_code
+ in (SELECT distinct agent_code from orders where amount >= 1000) 
+ group by agent_code ORDER BY MEANORDER DESC;
