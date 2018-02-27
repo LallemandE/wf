f9b994d9c61fab75a54a73fb680e8bcd0aeb0623
LallemandE <job@lallemand.lu>
2018-02-27 12:03:59 +0100

    exo 2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,12 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$middleOfPassword = floor(strlen($password)/2);
+
+$debut = substr($password, 0, $middleOfPassword);
+
+$fin = substr($password, $middleOfPassword, strlen($password)-$middleOfPassword);
+
+$saltedPassword = $debut . $salt . $fin;
+
+echo $saltedPassword;
