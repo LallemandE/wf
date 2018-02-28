f6e406d5beaa0ff199e69764bf977eb7229edb42
LallemandE <job@lallemand.lu>
2018-02-28 16:29:23 +0100

    exo6

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,23 @@
 <?php
+function easterReverse ($filename){
+    
+   $myResource = fopen($filename, 'r+');
+   $fileLength = filesize($filename);
+   $fileContent = fread($myResource, $fileLength);
+   
+   $positionInFile = floor(strlen($fileContent) / 2);
+   
+
+   
+   $firstPart = substr($fileContent, 0, $positionInFile);
+   $secondPart = strrev(substr($fileContent, $positionInFile, $fileLength-$positionInFile));
+   
+   // echo ("firstpart = $firstPart" . "\n\n\n");
+   // echo ("secondpart = $secondPart\n\n\n");
+   
+   fseek ($myResource, $positionInFile, SEEK_SET);
+   fwrite($myResource, $secondPart, strlen($secondPart));
+   
+   fclose($myResource);
+   
+}
