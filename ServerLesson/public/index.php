e6074006a634828e435a7635809c45ba67ae5372
LallemandE <job@lallemand.lu>
2018-03-10 16:06:19 +0100

    ajout de commentaire et petit bug corrigé dans index.php de public

--- ServerLesson/public/index.php
+++ ServerLesson/public/index.php
@@ -1,11 +1,21 @@
 <?php 
 
+// à partir du moment où l'on a utilisé le composer pour générer l'autoloader, il se trouve dans vendor
+
 require_once __DIR__. '/../vendor/autoload.php;'
+
+// on récupère le fichier de configuration de l'application (il ne doit donc plus apparaître dans les "controler" que l'on va "appeler"
 $configs = require __DIR__ . '/../config/app.conf.php';
 
-use Service\DBconnector;
 
+// on ouvre la connexion avec la DB (cela ne doit donc plus être fait dans les "controler" appelés
+use Service\DBconnector;
 DBConnector::setConfig($configs['db'];)
+
+// on crée une table de correspondance
+// à l'avenir, pour accéder au contrôler "account.php" qui se trouve dans src/controler, l'utilisateur va indiquer
+//        index.php/account comme URL
+// c'est dans le tableau suivant que l'on fait le lien entre ce qui suit "index.php/" et le vrai controler qu'il faut exécuter.
 
 
 $map = [
@@ -14,35 +24,41 @@ $map = [
     'register' => __DIR__. '/../src/Controller/register.php'
 ];
 
+// on récupère l'URI qui a été tapée par l'utilisateur
 
 $url = $SERVER['REQUEST_URI'];
 
+// ce qui nous intéresse est en fait la partie qui suit "index.php
 
 if (substr($url,0, strlen('/index.php'))== '/index.php'){
     $url=substr($url,strlen('/index.php'))
-} else if ($url =="/'){
+} else if ($url =="/"){
     $url = '';
 }
 
+// si ce qui suit index.php existe dans le tableau de correspondance défini ci dessus, on inclus le code de la page correspondante. 
 
 if (array_key_exists ($url, $map)) {
     include $map['url'];
 
 
-var_dump($url)
+// var_dump($url)
 
-// dans APACHE, je change les rÃ©pertoires pour qu'il aille par dÃ©faut dans public.
+// dans APACHE, je change les répertoires pour qu'il aille par défaut dans public.
 
 
-// SI Apache trouve une URL dans une URL, il l'exÃ©cute.
+// SI Apache trouve une URL dans une URL, il l'exécute.
+
+// Si Apache reçoit une URI du type www.salsanews.lu/index.php/quelque_chose_de_plus, il va traiter la page www.salsanews.lu/index.php
+// car c'est une URI totalement valide. C'est sur cette base qu'est construit ce programme-ci.
 
  /* 
     
-    Et aprÃ¨s ce code, il faut encore enlever l'include de init.php car c'est dÃ©jÃ  fait dans chacune des
+    Et après ce code, il faut encore enlever l'include de init.php car c'est déjà  fait dans chacune des
     pages inclues.
     
-    l'utilisateur est limitÃ© Ã  public et il ne peut plus accÃ©der Ã  des pages qui ne sont pas
-    prÃ©vues dans le mapping.
+    l'utilisateur est limité à  public et il ne peut plus accéder à  des pages qui ne sont pas
+    prévues dans le mapping.
     
     
 */
