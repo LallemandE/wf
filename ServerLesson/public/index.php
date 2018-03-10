f16bab01932bc62ade0d9c7d9c9ff82ecc103390
LallemandE <job@lallemand.lu>
2018-03-10 20:59:20 +0100

    correction d'accent dans le fichier index.php de public

--- ServerLesson/public/index.php
+++ ServerLesson/public/index.php
@@ -1,22 +1,22 @@
 <?php 
 
-// à partir du moment où l'on a utilisé le composer pour générer l'autoloader, il se trouve dans vendor
+// ï¿½ partir du moment oï¿½ l'on a utilisï¿½ le composer pour gï¿½nï¿½rer l'autoloader, il se trouve dans vendor
 
 require_once __DIR__. '/../vendor/autoload.php';
 
-// on récupère le fichier de configuration de l'application (il ne doit donc plus apparaître dans les "controler" que l'on va "appeler"
+// on rï¿½cupï¿½re le fichier de configuration de l'application (il ne doit donc plus apparaï¿½tre dans les "controler" que l'on va "appeler"
 
 $configs = require __DIR__ . '/../config/app.conf.php';
 
 
-// on ouvre la connexion avec la DB (cela ne doit donc plus être fait dans les "controler" appelés
+// on ouvre la connexion avec la DB (cela ne doit donc plus ï¿½tre fait dans les "controler" appelï¿½s
 use Service\DBconnector;
 DBConnector::setConfig($configs['db']);
 
-// on crée une table de correspondance
-// à l'avenir, pour accéder au contrôler "account.php" qui se trouve dans src/controler, l'utilisateur va indiquer
+// on crï¿½e une table de correspondance
+// ï¿½ l'avenir, pour accï¿½der au contrï¿½ler "account.php" qui se trouve dans src/controler, l'utilisateur va indiquer
 //        index.php/account comme URL
-// c'est dans le tableau suivant que l'on fait le lien entre ce qui suit "index.php/" et le vrai controler qu'il faut exécuter.
+// c'est dans le tableau suivant que l'on fait le lien entre ce qui suit "index.php/" et le vrai controler qu'il faut exï¿½cuter.
 
 
 $map = [
@@ -25,7 +25,7 @@ $map = [
     '/register' => __DIR__. '/../src/Controller/registernew.php'
 ];
 
-// on récupère l'URI qui a été tapée par l'utilisateur
+// on rï¿½cupï¿½re l'URI qui a ï¿½tï¿½ tapï¿½e par l'utilisateur
 
 $url = $_SERVER['REQUEST_URI'];
 
@@ -36,7 +36,7 @@ var_dump ($url);
 echo "<br>";
 */
 
-// ce qui nous intéresse est en fait la partie qui suit "index.php
+// ce qui nous intï¿½resse est en fait la partie qui suit "index.php
 
 
 if (substr($url,0, strlen('/index.php')) == '/index.php'){
@@ -53,14 +53,14 @@ echo "<br>";
 */
 
 
-// J'ai encore un petit soucis avec ce code dans la mesure où l'utilisateur pourrait déjà fournir des arguments dans le $_GET =>
-// Il faut que je récupère ce qui précède le ? possible de l'URI et c'est cette première partie qui constitue la clé de recherche dans la table de routage (map)
+// J'ai encore un petit soucis avec ce code dans la mesure oï¿½ l'utilisateur pourrait dï¿½jï¿½ fournir des arguments dans le $_GET =>
+// Il faut que je rÃ©cupÃ¨re ce qui prÃ©cÃ¨de le ? possible de l'URI et c'est cette premiï¿½re partie qui constitue la clï¿½ de recherche dans la table de routage (map)
 
 $requested_url = explode('?', $url, 2);
 
 
 
-// si ce qui suit index.php existe dans le tableau de correspondance défini ci dessus, on inclus le code de la page correspondante. 
+// si ce qui suit index.php existe dans le tableau de correspondance dï¿½fini ci dessus, on inclus le code de la page correspondante. 
 
 if (array_key_exists ($requested_url[0], $map)) {
     include $map[$requested_url[0]];
@@ -69,21 +69,21 @@ if (array_key_exists ($requested_url[0], $map)) {
 
 // var_dump($url)
 
-// dans APACHE, je change les répertoires pour qu'il aille par défaut dans public.
+// dans APACHE, je change les rï¿½pertoires pour qu'il aille par dï¿½faut dans public.
 
 
-// SI Apache trouve une URL dans une URL, il l'exécute.
+// SI Apache trouve une URL dans une URL, il l'exï¿½cute.
 
-// Si Apache reçoit une URI du type www.salsanews.lu/index.php/quelque_chose_de_plus, il va traiter la page www.salsanews.lu/index.php
+// Si Apache reï¿½oit une URI du type www.salsanews.lu/index.php/quelque_chose_de_plus, il va traiter la page www.salsanews.lu/index.php
 // car c'est une URI totalement valide. C'est sur cette base qu'est construit ce programme-ci.
 
  /* 
     
-    Et après ce code, il faut encore enlever l'include de init.php car c'est déjà  fait dans chacune des
+    Et aprï¿½s ce code, il faut encore enlever l'include de init.php car c'est dï¿½jï¿½ fait dans chacune des
     pages inclues.
     
-    l'utilisateur est limité à  public et il ne peut plus accéder à  des pages qui ne sont pas
-    prévues dans le mapping.
+    l'utilisateur est limitï¿½ ï¿½ public et il ne peut plus accï¿½der ï¿½ des pages qui ne sont pas
+    prï¿½vues dans le mapping.
     
     
 */
